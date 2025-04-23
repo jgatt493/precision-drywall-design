12cc9d679ed544e18f54d306886b7644108b6972
Jeremy Gatt <jpgatt06@gmail.com>
2025-04-23 01:21:10 -0400

    Updating

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -7,7 +7,7 @@ import { Label } from '@/components/ui/label';
 import { MapPin, Phone, Mail, Clock, Send } from 'lucide-react';
 import { useToast } from '@/components/ui/use-toast';
 
-const EDGE_FUNCTION_ENDPOINT = 'https://oodtywkclflclzeuctgu.supabase.co/functions/v1/send-email';
+const EDGE_FUNCTION_ENDPOINT = 'https://oodtywkclflclzeuctgu.supabase.co/functions/v1/super-endpoint'
 
 const Contact = () => {
     const { toast } = useToast();
